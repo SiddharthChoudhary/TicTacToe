63198514d94983687d5b88ba5150216a3728afb5
siddharth6 <siddharth@readybytes.in>
2018-09-19 08:45:56 -0400

    New Changes with saving in a file module working.

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,15 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import javax.sound.midi.SysexMessage;
 
 
 
@@ -11,26 +18,39 @@ public class TicTacToe {
 	static char playerArrays[] = new char[]{'X','O','A','B','C','D','E','F','G','H','I','J','K','L','M','N','P','Q','R','S','T','U','V','W','Y','Z'};
 	static int row;
 	static int column;
-	static int boardsize;
+    static int boardsize;
+    static int noOfPlayers;
+    static int winning_seq;
+    static TicTacToe t;
 	static int[][] matrix;
 	public static void main(String args[]){
-		int i=1;	
+        int i=1;	
 		try{
-		Scanner userInput = new Scanner(System.in);
-		Scanner userInputcolumn = new Scanner(System.in);
+        System.out.println("Start a new game or resume, r for resume, n for newgame");
+        Scanner startNewGameOrResume = new Scanner(System.in);
+        char choice = startNewGameOrResume.next().charAt(0);
+        if(choice=='r'){
+        System.out.println("Enter the filename");
+        Scanner filename = new Scanner(System.in);
+        int[][] matrixFromGame = resumeAgame(filename.next());
+        if(matrixFromGame!=null){
+            matrix=matrixFromGame; 
+            boardsize=matrixFromGame.length;
+            printBoard();
+        }
+        }
+        System.out.println("Starting a new game"); 
 		System.out.println("Enter size of the board");
 		Scanner sizeOfBoard = new Scanner(System.in);
 		boardsize     = sizeOfBoard.nextInt();
-		while(boardsize>999){
+        while(boardsize>999){
 			if(boardsize>999){
 		System.out.println("Boardsize is greater than 999, Try again");			
 		sizeOfBoard = new Scanner(System.in);
 		boardsize     = sizeOfBoard.nextInt();		
 			}
 		}
-		
-			 
-		TicTacToe t = new TicTacToe(boardsize	);
+		t = new TicTacToe(boardsize);
 		System.out.println("Enter number of Players");
 		Scanner NumberOfPlayers = new Scanner(System.in);
 		int noOfPlayers 		= NumberOfPlayers.nextInt();	 
@@ -44,26 +64,26 @@ public class TicTacToe {
 		System.out.println("Enter winning sequence");
 		Scanner winning_sequence = new Scanner(System.in);
 		int winning_seq 		= winning_sequence.nextInt();	 
-
-		while(true){
+        while(true){
 		for(i=0;i<noOfPlayers;i++){
             System.out.println();
+            quitGame();
             int[] rowColumnArray = new int[3];
-			System.out.println("Player "+playerArrays[i]);
-            try{
+            
+            System.out.println("Player "+playerArrays[i]);
             System.out.println("Enter row and column number");
             BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
             String  row_and_column = input.readLine();        
             String[] strings = row_and_column.trim().split("\\s+");
             for (int k = 0; k < strings.length; k++) {
-            rowColumnArray[k] = Integer.parseInt(strings[i]);
+            rowColumnArray[k] = Integer.parseInt(strings[k]);
             }
-            row   = (rowColumnArray[0]-1);
-            column= (rowColumnArray[1]-1);
-            System.out.println("Row and COlum"+rowColumnArray[0]+""+rowColumnArray[1]);
-        }catch(IOException e){
-            System.out.println("Try again, you have input something wrong!!");
-        }
+            if(rowColumnArray.length!=2){
+                row   = (rowColumnArray[0]-1);
+                column= (rowColumnArray[1]-1);
+            }else{
+                System.out.println("Try again with better inputs");
+            }
             if(row<boardsize&&column<boardsize){
 				winner = t.move(row,column,i,winning_seq);
 				printBoard();
@@ -81,11 +101,10 @@ public class TicTacToe {
 			}
 	}
 
-		System.out.println("The winner is Player "+playerArrays[i]);
-		userInput.close();
-		userInputcolumn.close();		
-		}catch(InputMismatchException e){
-			System.out.println("Wrong Input type");
+		System.out.println("The winner is Player "+playerArrays[i]);		
+		}catch(IOException|ArrayIndexOutOfBoundsException|InputMismatchException e){
+            e.printStackTrace();
+            System.out.println("Wrong Input type");
 
 		}
 		}
@@ -107,7 +126,6 @@ public class TicTacToe {
 			System.out.print(" _ _ ");
 		}}
 		System.out.println();
-	
 		
 	}
 }
@@ -119,16 +137,8 @@ public class TicTacToe {
 				matrix[i][j]=-1;
 			}
 		}
-		}
-	
-	   /** Player {player} makes a move at ({row}, {col}).
-		   @param row The row of the board.
-		   @param col The column of the board.
-		   @param player The player, can be either 1 or 2.
-		   @return The current winning condition, can be either:
-				   0: No one wins.
-				   1: Player 1 wins.
-				   2: Player 2 wins. */
+        }
+
 	   public int move(int row, int col, int player,int winning_sequence) {	
         matrix[row][col]=player;
         int right_count = checkright(row,col,player,0);
@@ -139,21 +149,16 @@ public class TicTacToe {
         int top_right = top_right(row,col,player,0);
         int down_left= down_left(row, col, player, 0);
         int down_right=down_right(row, col, player, 0);
-        System.out.println("COunt is"+left_count);
         if((left_count+right_count-1)==winning_sequence){
-         System.out.println("COunt is"+(left_count+right_count));
             return player;
         }
         if((top_count+down_count-1)==winning_sequence){
-            System.out.println("COunt is"+(top_count+down_count));
                return player;
         }
         if((top_right+down_left-1)==winning_sequence){
-            System.out.println("COunt is"+(top_right+down_left));
                return player;
         }
         if((top_left+down_right-1)==winning_sequence){
-            System.out.println("COunt is"+(top_left+down_right));
                return player;
         }
         return -1;
@@ -222,4 +227,93 @@ public class TicTacToe {
             }
                 return count;
             }
+        public static boolean saveInfile(int[][] matrix,String path, String filename){
+           try{
+            StringBuilder builder = new StringBuilder();
+            for(int i = 0; i < matrix.length; i++)//for each row
+            {
+               for(int j = 0; j < matrix.length; j++)//for each column
+               {
+                  builder.append(matrix[i][j]+"");//append to the output string
+                  if(j < matrix.length - 1)//if this is not the last row element
+                     builder.append(",");//then add comma (if you don't like commas you can use spaces)
+               }
+               builder.append("\n");//append new line at the end of the row
+            }
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File(filename + ".txt")));
+            writer.write(builder.toString());//save the string representation of the board
+            writer.close();
+            return true;
+        }catch(Exception io){
+            io.printStackTrace();
+            System.out.println("Input Output Exception caught");
+            return false;
+        }
+        }
+        public static int quitGame(){
+            System.out.println("Do you want to quit the game? Press q for quit, n for Don't quit and continue");
+            Scanner quitOrNot = new Scanner(System.in);
+            char ch = quitOrNot.next().charAt(0);
+            if(ch=='q'){
+                System.out.println("Do you want to save it (s for save, n for don't save and just quit, c for cancel and back to previous question)?"); 
+                Scanner saveOrNot = new Scanner(System.in);
+                char saveOrnot = saveOrNot.next().charAt(0);
+                if(saveOrnot=='s'){
+                    System.out.println("Enter filename for the saving the file");
+                    Scanner filenameInput = new Scanner(System.in);
+                    String filename    =   filenameInput.next();
+                    if(saveInfile(matrix,"/",filename)){
+                        System.out.println("Successfully saved");
+                        System.exit(0);
+                    }else{
+                        System.out.println("Couldn't save successfully");
+                        return -1;
+                    }
+                }
+                if(saveOrnot=='n'){
+                    System.out.println("Now exiting...");    
+                    System.exit(0);
+                }
+                if(saveOrnot=='c'){
+                    quitGame();
+                }
+            }
+            if(ch=='n'){
+               return -1;
+            }
+            return -1;
+        }
+        public static int[][] resumeAgame(String filename){
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(filename+".txt"));
+            String line = "";
+            int row = 0,column=0;
+            while((line = reader.readLine()) != null)
+            {   
+               String[] cols = line.split(","); //note that if you have used space as separator you have to split on " "
+               column = cols.length;
+               row++;
+            }
+            int[][] board = new int[row][column];            
+            BufferedReader readeragain = new BufferedReader(new FileReader(filename+".txt"));
+            line = "";
+            row=0;   
+            while((line = readeragain.readLine()) != null)
+                {               
+                    String[] cols = line.split(","); //note that if you have used space as separator you have to split on " "
+                    int col = 0;
+                    for(String  c : cols)
+                    {
+                    board[row][col] = Integer.parseInt(c);
+                    col++;
+                    }
+                    row++;
+                }
+                    reader.close();
+                    return board;
+        }catch(IOException e){
+            System.out.println("Can't resume, Couldn't find the file");
+            return null;
+        }
+    }
 }
